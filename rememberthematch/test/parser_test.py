dfb54d56b8079c7b84e72c019815a1d905179eb8
Michal Michalski <github@michalski.im>
2014-07-13 23:25:20 +0100

    Add test for Premier League parser

--- rememberthematch/test/parser_test.py
+++ rememberthematch/test/parser_test.py
@@ -1,7 +1,29 @@
+import os
 import unittest
+from mockito import *
 from rememberthematch.parser import PremierLeagueHTMLParser
+from rememberthematch.urldownloader import UrlDownloader
 
 
-class ParserTest(unittest.TestCase):
-    def test(self):
-        pass
+class PremierLeagueHTMLParserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = PremierLeagueHTMLParser()
+
+    def tearDown(self):
+        unstub()
+
+    def get_path(self, filename):
+        dir = os.path.dirname(__file__)
+        return os.path.join(dir, filename)
+
+    def testParseSampleHtmlSuccessfully(self):
+        downloader = mock(UrlDownloader)
+        with open(self.get_path("data/premierleague.html")) as f:
+            static_html = f.read()
+            when(downloader).download().thenReturn(static_html)
+            output = self.parser.parse()
+            timestamps = [int(ts) for ts in output.keys()]
+            self.assertEqual(1432425600, max(timestamps))
+            self.assertEqual(1408147200, min(timestamps))
+            self.assertEqual(41, len(output))
