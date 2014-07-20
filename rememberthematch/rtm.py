99fa2989b30cf11b41a62296d63e3508a6d3d1fd
Michal Michalski <github@michalski.im>
2014-07-20 16:41:39 +0100

    Refactor RememberTheMatch main class

--- rememberthematch/rtm.py
+++ rememberthematch/rtm.py
@@ -6,6 +6,8 @@ from todoclient import TodoistClient
 
 class RememberTheMatch(object):
 
+    TASK_NAME_FORMAT = "%s vs. %s at %s"
+
     def __init__(self, username, password, project, interactive=True, dry_run=True):
         self.logger = logging.getLogger(__name__)
         self.parser = PremierLeagueHTMLParser()
@@ -15,22 +17,24 @@ class RememberTheMatch(object):
         self.interactive = interactive
         self.dry_run = dry_run
 
+        # TODO: needs to be dynamic pasing on script arguments
+        self.client = TodoistClient(self.username, self.password, self.project)
+
     def print_schedule(self, matches):
         for match in matches:
             self.logger.info("%(timestamp)s: %(homeTeamName)s vs. %(awayTeamName)s at %(venue)s" % match)
 
-    def run(self):
-        matches = self.parser.parse()
-        self.print_schedule(matches)
-
-        # TODO: needs to be dynamic pasing on script arguments
-        todoist_client = TodoistClient(self.username, self.password, self.project)
-
+    def add_tasks(self, matches):
         for match in matches:
             timestamp = match['timestamp']
             home_team = match['homeTeamName']
             away_team = match['awayTeamName']
             venue = match['venue']['name']
 
-            task_name = "%s vs. %s at %s" % (home_team, away_team, venue)
-            todoist_client.add_task(timestamp, task_name, venue)
+            task_name = self.TASK_NAME_FORMAT % (home_team, away_team, venue)
+            self.client.add_task(timestamp, task_name, venue)
+
+    def run(self):
+        matches = self.parser.parse()
+        self.print_schedule(matches)
+        self.add_tasks(matches)
