a529ccd975fcc3d126cb247e1cbd8b4c86312db7
Michal Michalski <github@michalski.im>
2014-07-26 19:59:50 +0100

    Changes to Match implementation, tests

--- rememberthematch/match.py
+++ rememberthematch/match.py
@@ -1,7 +1,26 @@
+from datetime import datetime
+
+
 class Match(object):
+
+    DATE_FORMAT = "%Y-%m-%d"
 
     def __init__(self, timestamp, home_team, away_team, venue):
         self.timestamp = timestamp
         self.home_team = home_team
         self.away_team = away_team
         self.venue = venue
+
+    @property
+    def date(self):
+        return datetime.fromtimestamp(self.timestamp).strftime(self.DATE_FORMAT)
+
+    def __eq__(self, other):
+        return self.timestamp == other.timestamp and \
+            self.home_team == other.home_team and \
+            self.away_team == other.away_team and \
+            self.venue == other.venue
+
+    def __str__(self):
+        return "Match(timestamp=%d [%s], home_team=%s, away_team=%s, venue=%s)" % \
+            (self.timestamp, self.date, self.home_team, self.away_team, self.venue)
---
+++ rememberthematch/test/match_test.py
@@ -0,0 +1,28 @@
+import unittest
+from rememberthematch.match import Match
+
+
+class MatchTest(unittest.TestCase):
+
+    def test_matches_are_equal(self):
+        m1 = Match(1, "teamA", "teamB", "venue")
+        m2 = Match(1, "teamA", "teamB", "venue")
+
+        self.assertEqual(m1, m2)
+
+    def test_matches_are_not_equal(self):
+        m = Match(1, "teamA", "teamB", "venue")
+        m1 = Match(2, "teamA", "teamB", "venue")
+        m2 = Match(1, "teamX", "teamB", "venue")
+        m3 = Match(1, "teamA", "teamX", "venue")
+        m4 = Match(1, "teamA", "teamB", "venueX")
+
+        self.assertNotEqual(m, m1)
+        self.assertNotEqual(m, m2)
+        self.assertNotEqual(m, m3)
+        self.assertNotEqual(m, m4)
+
+    def test_match_get_date(self):
+        m = Match(1, "teamA", "teamB", "venue")
+
+        self.assertEqual(m.date, "1970-01-01")
