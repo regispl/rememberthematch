e2b88c7bfc43b70d3ccc5847317b65d4d0c0c276
Michal Michalski <github@michalski.im>
2014-07-26 20:01:56 +0100

    DateFilter changes

--- rememberthematch/test/filter_test.py
+++ rememberthematch/test/filter_test.py
@@ -1,16 +1,13 @@
+import time
 import unittest
 
+from datetime import datetime
+
 from rememberthematch.match import Match
-from rememberthematch.filter import TeamNameFilter
+from rememberthematch.filter import TeamNameFilter, DateFilter
 
 
 class TeamNameFilterTest(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def get_matches(self):
         m1 = Match(1, "teamA", "teamB", "venue1")
@@ -21,19 +18,79 @@ class TeamNameFilterTest(unittest.TestCase):
         return matches
 
     def test_filter_matches(self):
-        name = "teamA"
-        filter = TeamNameFilter([name])
+        f = TeamNameFilter(["teamA"])
         matches = self.get_matches()
-        matches = filter.filter(matches)
+        matches_f = f.filter(matches)
 
-        self.assertEquals(2, len(matches))
-        self.assertEquals(name, matches[0].home_team)
-        self.assertEquals(name, matches[1].away_team)
+        self.assertEquals(2, len(matches_f))
+        self.assertEquals(matches[0], matches_f[0])
+        self.assertEquals(matches[2], matches_f[1])
 
     def test_filter_no_matches(self):
-        name = "teamX"
-        filter = TeamNameFilter([name])
+        f = TeamNameFilter("teamX")
         matches = self.get_matches()
-        matches = filter.filter(matches)
+        matches_f = f.filter(matches)
 
-        self.assertEquals(0, len(matches))
+        self.assertEquals(0, len(matches_f))
+
+
+class DateFilterTest(unittest.TestCase):
+
+    def get_timestamp(self, date):
+        return time.mktime(datetime.strptime(date, "%Y-%m-%d").timetuple())
+
+    def get_matches(self):
+        m1 = Match(self.get_timestamp("2014-07-26"), "teamA", "teamB", "venue1")
+        m2 = Match(self.get_timestamp("2014-08-16"), "teamB", "teamC", "venue2")
+        m3 = Match(self.get_timestamp("2014-11-11"), "teamC", "teamA", "venue3")
+        m4 = Match(self.get_timestamp("2015-01-26"), "teamD", "teamC", "venue4")
+        m5 = Match(self.get_timestamp("2015-05-04"), "teamA", "teamD", "venue1")
+        matches = [m1, m2, m3, m4, m5]
+
+        return matches
+
+    def test_filter_min_date_only(self):
+        min_date = "2014-11-11"
+        f = DateFilter(min=min_date)
+        matches = self.get_matches()
+        matches_f = f.filter(matches)
+
+        self.assertEquals(3, len(matches_f))
+        self.assertEqual(matches[2], matches_f[0])
+        self.assertEqual(matches[3], matches_f[1])
+        self.assertEqual(matches[4], matches_f[2])
+
+    def test_filter_max_date_only(self):
+        max_date = "2014-11-11"
+        f = DateFilter(max=max_date)
+        matches = self.get_matches()
+        matches_f = f.filter(matches)
+
+        self.assertEquals(3, len(matches_f))
+        self.assertEqual(matches[0], matches_f[0])
+        self.assertEqual(matches[1], matches_f[1])
+        self.assertEqual(matches[2], matches_f[2])
+
+    def test_filter_min_and_max_date(self):
+        min_date = "2014-08-16"
+        max_date = "2015-01-26"
+        f = DateFilter(min=min_date, max=max_date)
+        matches = self.get_matches()
+        matches_f = f.filter(matches)
+
+        self.assertEquals(3, len(matches_f))
+        self.assertEqual(matches[1], matches_f[0])
+        self.assertEqual(matches[2], matches_f[1])
+        self.assertEqual(matches[3], matches_f[2])
+
+    def test_filter_no_dates(self):
+        f = DateFilter()
+        matches = self.get_matches()
+        matches_f = f.filter(matches)
+
+        self.assertEquals(5, len(matches_f))
+        self.assertEqual(matches[0], matches_f[0])
+        self.assertEqual(matches[1], matches_f[1])
+        self.assertEqual(matches[2], matches_f[2])
+        self.assertEqual(matches[3], matches_f[3])
+        self.assertEqual(matches[4], matches_f[4])
