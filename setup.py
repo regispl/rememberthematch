124db991ea4433d2fbabe2618dde9e8a007db405
Michał Michalski <github@michalski.im>
2015-09-18 19:57:28 +0100

    Workaround for required session parameter in pip 6.0+

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from rememberthematch import __version__ as version
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-install_reqs = parse_requirements(os.path.join(here, "requirements.txt"))
+install_reqs = parse_requirements(os.path.join(here, "requirements.txt"), session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
